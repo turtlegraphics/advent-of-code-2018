6a5a69f7aa45d09bf76b18b502093f9344553955
Bryan Clair <bryan@slu.edu>
2018-12-08 23:57:18 -0600

    Ahhh. Linked list pays off in part 2.

--- day9/solve.py
+++ day9/solve.py
@@ -5,22 +5,26 @@ if len(sys.argv) != 3:
     sys.exit()
 
 players = int(sys.argv[1])
-marbles = int(sys.argv[2])    
+marbles = int(sys.argv[2])
 
 print players,'players',marbles,'marbles'
 
 class Marble:
     def __init__(self,val):
         self.val = val
-        self.next = None
+        self.next = self
+        self.prev = self
 
     def __str__(self):
         return str(self.val)
 
+    def is_special(self):
+        return self.val % 23 == 0
+
 class Circle:
     def __init__(self):
-        self.first = None
-        self.current = None
+        self.first = Marble(0)
+        self.current = self.first
 
     def __str__(self):
         if self.first == None:
@@ -45,18 +49,32 @@ class Circle:
         return out
 
     def add(self,marble):
-        if self.current == None:
-            self.first = marble
-            self.current = marble
-            marble.next = marble
-            return
+        if marble.is_special():
+            score = marble.val
+            take = self.current.prev.prev.prev.prev.prev.prev.prev
+            score += take.val
+            self.current = take.next
+            take.prev.next = take.next
+            take.next.prev = take.prev
+            return score
 
+        # not special :-(
         spot = self.current.next
+        spot.next.prev = marble
         marble.next = spot.next
+        marble.prev = spot
         spot.next = marble
         self.current = marble
 
+        return 0 # no score
+
 game = Circle()
-for m in range(marbles):
-    game.add(Marble(m))
-    print game
+player = 0
+scores = [0]*players
+
+for m in range(marbles+1)[1:]:
+   scores[player] += game.add(Marble(m))
+   player = (player + 1) % players
+
+print scores
+print max(scores)
