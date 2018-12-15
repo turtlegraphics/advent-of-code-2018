02fa0c2a3283d05d8c232ad3bea621e70e23c393
Bryan Clair <bryan@slu.edu>
2018-12-15 11:49:46 -0800

    Working version. Well, its fun anyway.

--- day15/solve.py
+++ day15/solve.py
@@ -1,32 +1,28 @@
 import sys
 from collections import deque
-import color
-
-filename = 'input.txt'
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-
-data = open(filename).readlines()
-
-races = ['E','G']
+from box import *
 
 class Game:
     def __init__(self,lines):
         self.board = []
+        self.combatants = {Elf:0, Gnome:0}
         for line in lines:
-            line = line.strip()
-            self.board.append(list(line))
+            row = []
+            for c in line.strip():
+                if c == '#':
+                    row.append(Wall())
+                elif c == '.':
+                    row.append(Empty())
+                elif c == 'E':
+                    row.append(Elf())
+                    self.combatants[Elf] += 1
+                elif c == 'G':
+                    row.append(Gnome())
+                    self.combatants[Gnome] += 1
+
+            self.board.append(row)
         self.width = len(self.board[0])
         self.height = len(lines)
-
-        self.hp = [[0]*self.width for y in range(self.height)]
-
-        for p in self:
-            if self[p] in races:
-                self.hp[p[0]][p[1]] = min(p[1]*10,200)  # change to 200
-
-        print self.hp
-
 
     def __getitem__(self, pos):
         """Return the board at position pos = (y,x)"""
@@ -41,60 +37,52 @@ class Game:
             for x in range(self.width):
                 yield( (y,x) )
 
+    def units(self,race):
+        """Iterate over units of a given race: Elf, Gnome, or Unit = both."""
+        for pos in self:
+            if isinstance(self[pos],race):
+                yield(pos)
+
     def neighbors(self,pos):
         """Return a list of valid neighbors of pos"""
         (y,x) = pos
         n = []
-        if y-1 > 0 and self[y-1,x] != '#':
+        if y-1 > 0 and not isinstance(self[y-1,x],Wall):
             n.append( (y-1,x) )
-        if x-1 > 0 and self[y,x-1] != '#':
+        if x-1 > 0 and not isinstance(self[y,x-1],Wall):
             n.append( (y,x-1) )
-        if x+1 < self.width and self[y,x+1] != '#':
+        if x+1 < self.width and not isinstance(self[y,x+1],Wall):
             n.append( (y,x+1) )
-        if y+1 < self.height and self[y+1,x] != '#':
+        if y+1 < self.height and not isinstance(self[y+1,x],Wall):
             n.append( (y+1,x) )
         return n
 
     def __str__(self):
-        out = str(self.width) + 'x' + str(self.height)
+        out = 'Elves: '+str(self.combatants[Elf])
+        out += '\tGnomes: '+str(self.combatants[Gnome])
         for y in range(self.height):
             out += '\n'
             for x in range(self.width):
                 v = self[y,x]
-                if v in races:
-                    c = color.fade(v,self.hp[y][x])
-                elif isinstance(v,int):
-                    if v >= 10:
-                        c = 'X'
-                    else:
-                        c = str(v)
+                if isinstance(v,int) and v >= 10:
+                    c = 'X'
                 else:
-                    c = v
+                    c = str(v)
                 out += c
         return out
 
-    def empty(self,pos):
-        return self[pos] == '.'
-
-    def units(self,which):
-        """return a list of units, which is a list of types to find"""
-        found = []
-        for pos in self:
-            if self[pos] in which:
-                found.append(pos)
-        return found
-
-    def inrange(self,which):
-        for u in self.units(which):
+    def inrange(self,race):
+        """Mark board with squares one away from all units of type race"""
+        for u in self.units(race):
             for n in self.neighbors(u):
-                if self.empty(n):
+                if isinstance(self[n],Empty):
                     self[n] = '?'
 
     def clean(self):
         """Clean up a messy board."""
         for pos in self:
-            if self[pos] not in ['#','E','G']:
-                self[pos] = '.'
+            if not isinstance(self[pos],Box):
+                self[pos] = Empty()
 
     def distances(self,startpos):
         """mark board with distances from startpos.  returns a list
@@ -108,7 +96,7 @@ class Game:
             val = self[pos]
             for n in self.neighbors(pos):
                 vn = self[n]
-                if vn == '.':
+                if isinstance(vn,Empty):
                     self[n] = val+1
                     visit.append(n)
                 elif isinstance(vn,int):
@@ -135,8 +123,7 @@ class Game:
         """Return a move for the unit at startpos, or None."""
         # where do I want to be
         me = self[startpos]
-        enemy = 'G' if me == 'E' else 'E'
-        self.inrange(enemy)  # mark board with ?'s for in range squares
+        self.inrange(me.enemy())  # mark board with ?'s for in range squares
         chosen = self.choose(startpos)
         if chosen == None:
             return None
@@ -151,27 +138,66 @@ class Game:
         self.clean()
 
         # make the move
-        assert(self[pos] == '.')
-        self[startpos] = '.'
+        assert(isinstance(self[pos],Empty))
+        self[startpos] = Empty()
         self[pos] = me
+        me.moved = True
+
         # try to attack
         self.attack(pos)
 
     def attack(self,startpos):
         """Make an attack if possible. Return True if attack happened."""
         me = self[startpos]
-        enemy = 'G' if me == 'E' else 'E'
         for n in self.neighbors(startpos):
-            if self[n] == enemy:
-                print me,'at',startpos,'attacked',enemy,'at',n
+            if isinstance(self[n],me.enemy()):
+#                print me,'at',startpos,'attacked',self[n],'at',n
+                killed = self[n].hurt()
+                if killed:
+#                    print me,'killed',self[n],'!!!!'
+                    self.combatants[me.enemy()] -= 1
+                    self[n] = Empty()
                 return True
         return False
 
+    def victor(self):
+        if self.combatants[Elf] == 0:
+            return Gnome
+        elif self.combatants[Gnome] == 0:
+            return Elf
+        return None
+
+filename = 'input.txt'
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
+data = open(filename).readlines()
+
 g  = Game(data)
+round = 0
 while True:
+    print 'round',round
     print(g)
-    raw_input()
-    for u in g.units(['E','G']):
-        if not g.attack(u):
-            g.move(u)
-            
+    for u in g.units(Unit):
+        if not g[u].moved:
+            if not g.attack(u):
+                g.move(u)
+        if g.victor():
+            break
+    if g.victor():
+        break
+
+    for u in g.units(Unit):
+        g[u].moved = False
+
+    round += 1
+
+
+print '='*30
+print 'Elves win!' if g.victor() == Elf else 'Gnomes win!'
+print(g)
+
+totalhp = 0
+for u in g.units(g.victor()):
+    totalhp += g[u].hp
+print 'Outcome:',round,'*',totalhp,'=',round*totalhp
