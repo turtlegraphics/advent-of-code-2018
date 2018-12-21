7e3ece4b389ae316f71c7875f913561b79a9e818
Bryan Clair <bryan@slu.edu>
2018-12-20 22:54:16 -0600

    showing walls

--- day20/Board.py
+++ day20/Board.py
@@ -1,16 +1,29 @@
 class Cell:
-    def __init__(self,loc,val='.'):
-        self.val = val
-        self.neighbors = [None]*4
+    def __init__(self,loc):
+        self.neighbors = {'N':None,'S':None,'E':None,'W':None}
         self.loc = loc
 
     def __str__(self):
         return self.val
 
 class Board:
+    delta = {'N':(0,-1),'S':(0,1),'E':(1,0),'W':(-1,0)}
+    opposite = {'N':'S','S':'N','E':'W','W':'E'}
     def __init__(self):
-        self.start = Cell((0,0),'X')
-        self.cells = {(0,0):self.start}
+        self.cells = {(0,0):Cell((0,0))}
+        self.position = (0,0)
+
+    def move(self,direction):
+        (x,y) = self.position
+        (dx,dy) = Board.delta[direction]
+        newpos = (x+dx,y+dy)
+        if newpos not in self.cells:
+            self.cells[newpos] = Cell(newpos)
+
+        self.cells[newpos].neighbors[Board.opposite[direction]] = self.position
+        self.cells[self.position].neighbors[direction] = newpos
+        
+        self.position = newpos
 
     def __str__(self):
         minx = 0
@@ -28,14 +41,38 @@ class Board:
                 maxy = y
         out = ''
         for y in range(miny,maxy+1):
+            above = ''
+            row = ''
             for x in range(minx,maxx+1):
+                above += '#'
                 if (x,y) in self.cells:
-                    out += str(self.cells[(x,y)])
-            out += '\n'
-        return out[:-1]
+                    if self.cells[(x,y)].neighbors['W']:
+                        row += ' '
+                    else:
+                        row += '?'
+
+                    if self.cells[(x,y)].neighbors['N']:
+                        above += ' '
+                    else:
+                        above += '?'
+
+                    row += 'X' if self.position == (x,y) else 'o'
+                else:
+                    above += '?'
+                    row += '??'
+            above += '#\n'
+            row += '?\n'
+            out += above + row
+        out += '#'+'?#'*(maxx-minx+1)
+        return out
 
 if __name__=='__main__':
     b = Board()
+    b.move('N')
+    b.move('E')
+    b.move('S')
+    b.move('W')
+    b.move('W')
     print b
 
 
